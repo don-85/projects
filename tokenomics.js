7c99fea68a6e85d71e5fbe5a7f15c62d92711ae0
don-85 <muntutitus17@gmail.com>
2024-12-14 00:23:16 +0300

    updated the RVNL site

---
+++ tokenomics.js
@@ -0,0 +1,45 @@
+ var ctx = document.getElementById('tokenomicsChart').getContext('2d');
+        var tokenomicsChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Liquidity Pool', 'Development Fund', 'Community Rewards', 'Marketing and Partnerships', 'Team Reserve'],
+                datasets: [{
+                    label: 'Token Distribution',
+                    data: [30, 20, 25, 15, 10],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    tooltip: {
+                        callbacks: {
+                            label: function(tooltipItem) {
+                                let sum = 0;
+                                let dataArr = tooltipItem.dataset.data;
+                                dataArr.forEach(data => sum += data);
+                                let percentage = (tooltipItem.raw / sum * 100).toFixed(1);
+                                return `${tooltipItem.label}: ${percentage}%`;
+                            }
+                        }
+                    }
+                }
+            }
+        });
